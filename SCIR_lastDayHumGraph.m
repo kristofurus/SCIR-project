565ce47449b160543f3cf244ad5560894e351189
kristofurus <krzysztof.ladaa@gmail.com>
2023-01-06 12:19:01 +0100

    Reformat plot last 24h to use dateshift function

--- SCIR_lastDayHumGraph.m
+++ SCIR_lastDayHumGraph.m
@@ -6,78 +6,56 @@ humidityFieldID = [2 5 7];
 internetChannel = 1890915;
 internetChannelReadKey = 'CT8EQRAQBZGWLM2L';
 
-NUM_OF_HOURS = 25;
+NUM_OF_HOURS = 24;
 NUM_OF_SENSORS = size(humidityFieldID, 2);
 
 % get last 24 values from internet data
 [internet_hum, internet_times] = thingSpeakRead(internetChannel, 'Fields', 2, 'NumMinutes', NUM_OF_HOURS*60, 'ReadKey', internetChannelReadKey);
 last_hour = hour(internet_times(end));
 
-% current time
-current_time = datetime('now');
-additional_minutes = round(minutes(current_time-internet_times(end)));
-
 % get last whole 24 hours
-[hum, time] = thingSpeakRead(sensorChannelID, 'Fields', humidityFieldID, 'NumMinutes', NUM_OF_HOURS*60+additional_minutes, 'ReadKey', sensorChannelReadKey);
+[hum, time] = thingSpeakRead(sensorChannelID, 'Fields', humidityFieldID, 'NumMinutes', NUM_OF_HOURS*60, 'ReadKey', sensorChannelReadKey);
 
-% average_humeratures = zeros(4, NUM_OF_HOURS);
-% std_hum = zeros(4, NUM_OF_HOURS);
+dates_int = dateshift(internet_times, 'start', 'hour');
+dates = dateshift(time, 'start', 'hour');
+dates_unique = unique(dates);
 
-times = internet_times;
-start = datetime('yesterday');
-start = start + hours(last_hour);
+pos_err = zeros(size(dates_unique, 1), NUM_OF_SENSORS);
+median_hum = zeros(size(dates_unique, 1), NUM_OF_SENSORS);
+neg_err = zeros(size(dates_unique, 1), NUM_OF_SENSORS);
 
-pos_err = zeros(NUM_OF_HOURS, NUM_OF_SENSORS);
-median_hum = zeros(NUM_OF_HOURS, NUM_OF_SENSORS);
-neg_err = zeros(NUM_OF_HOURS, NUM_OF_SENSORS);
-
-plot_internet_hum = zeros(NUM_OF_HOURS, 1); 
-
-for i = 0:(NUM_OF_HOURS-1)
+for i = 1:size(dates_unique, 1)
     
-    % get data from selected hour
-    hourly_hum = hum(time >= start+hours(i) & time < start+hours(i + 1), :);
-    times(i+1) = start+hours(i) + minutes(30);
-    
-    plot_internet_hum(i+1) = mean(internet_hum(internet_times >= start+hours(i) & internet_times < start+hours(i + 1)));
-    
-    % find Q1, Q2 Q3
+    selected_hum = hum(dates == dates_unique(i), :);
+
     for j = 1:NUM_OF_SENSORS
         
-       sensor = hourly_hum(~isnan(hourly_hum(:, j)), j);
+        sensor = selected_hum(~isnan(selected_hum(:, j)), j);
        
-       % get Q1, Q2, Q3
-       q1 = prctile(sensor, 25);
-       median_hum(i+1, j) = median(sensor);
-       q3 = prctile(sensor, 75);
+        % get Q1, Q2, Q3
+        q1 = prctile(sensor, 25);
+        median_hum(i, j) = median(sensor);
+        q3 = prctile(sensor, 75);
  
-       if ~isempty(sensor)
-           
-           % get IRQ
-           IRQ = abs(q3-q1);
+        if ~isempty(sensor)
 
-           pos_err(i+1, j) = abs(median_hum(i+1, j) - max(sensor(sensor <= q3+3/2*IRQ)));
-           neg_err(i+1, j) = abs(median_hum(i+1, j) - min(sensor(sensor >= q1-3/2*IRQ)));
-       end
-       
+            % get IRQ
+            IRQ = abs(q3-q1);
+            pos_err(i, j) = abs(median_hum(i, j) - max(sensor(sensor <= q3+3/2*IRQ)));
+            neg_err(i, j) = abs(median_hum(i, j) - min(sensor(sensor >= q1-3/2*IRQ)));
+        
+        end
     end
-    
 end
 
 % plot data
-b = bar(times, plot_internet_hum, 1, "FaceAlpha",0.5, 'linestyle', '--');
-% b.BaseValue = 50;
-% a = area(times, internet_hum,"FaceAlpha",0.5, 'linestyle', '--');
-% a.BaseValue = 50;
-% plot(times, plot_internet_hum, "color", "#4DBEEE", "linewidth", 20);  
-% bar(times, internet_hum, 'linestyle', '--', "linewidth", 1.5); 
+stairs(dates_int, internet_hum , "--", "color", "#000000", "linewidth", 2);  
 hold on
-% errorbar([times,times,times,times], median_hum,lower_outlier, upper_outlier, '-o', "linewidth", 1.5, "MarkerSize",8, "capsize", 10);
-errorbar(repmat(times, [1, NUM_OF_SENSORS]), median_hum,neg_err, pos_err, '-o', "linewidth", 1.5, "MarkerSize",8, "capsize", 10);
+errorbar(repmat(dates_unique, [1, NUM_OF_SENSORS]), median_hum, neg_err, pos_err, '-o', "linewidth", 1.5, "MarkerSize",8, "capsize", 10);
 hold off
 grid minor
 xlabel("date");
-xticks(times(1:3:NUM_OF_HOURS));
+xticks(dates_unique(1:3:NUM_OF_HOURS));
 ylabel("Humidity [%]");
 legend(["Internet", "SHT31", "DHT11", "DHT22"], "Location", "eastoutside")
 title("Last 24 hour humidity");
--- SCIR_lastDayTempGraph.m
+++ SCIR_lastDayTempGraph.m
@@ -6,75 +6,56 @@ temperatureFieldID = [1 3 4 6 8];
 internetChannel = 1890915;
 internetChannelReadKey = 'CT8EQRAQBZGWLM2L';
 
-NUM_OF_HOURS = 25;
+NUM_OF_HOURS = 24;
 NUM_OF_SENSORS = size(temperatureFieldID, 2);
 
 % get last 24 values from internet data
-% [internet_temp, internet_times] = thingSpeakRead(internetChannel, 'Fields', 1, 'NumPoints', NUM_OF_HOURS, 'ReadKey', internetChannelReadKey);
 [internet_temp, internet_times] = thingSpeakRead(internetChannel, 'Fields', 1, 'NumMinutes', NUM_OF_HOURS*60, 'ReadKey', internetChannelReadKey);
 last_hour = hour(internet_times(end));
 
-% current time
-current_time = datetime('now');
-additional_minutes = round(minutes(current_time-internet_times(end)));
-
 % get last whole 24 hours
-[temp, time] = thingSpeakRead(sensorChannelID, 'Fields', temperatureFieldID, 'NumMinutes', NUM_OF_HOURS*60+additional_minutes, 'ReadKey', sensorChannelReadKey);
+[temp, time] = thingSpeakRead(sensorChannelID, 'Fields', temperatureFieldID, 'NumMinutes', NUM_OF_HOURS*60, 'ReadKey', sensorChannelReadKey);
 
-% average_temperatures = zeros(4, NUM_OF_HOURS);
-% std_temp = zeros(4, NUM_OF_HOURS);
+dates_int = dateshift(internet_times, 'start', 'hour');
+dates = dateshift(time, 'start', 'hour');
+dates_unique = unique(dates);
 
-times = internet_times;
-start = datetime('yesterday');
-start = start + hours(last_hour);
+pos_err = zeros(size(dates_unique, 1), NUM_OF_SENSORS);
+median_temp = zeros(size(dates_unique, 1), NUM_OF_SENSORS);
+neg_err = zeros(size(dates_unique, 1), NUM_OF_SENSORS);
 
-pos_err = zeros(NUM_OF_HOURS, NUM_OF_SENSORS);
-median_temp = zeros(NUM_OF_HOURS, NUM_OF_SENSORS);
-neg_err = zeros(NUM_OF_HOURS, NUM_OF_SENSORS);
-
-plot_internet_temp = zeros(NUM_OF_HOURS, 1); 
-
-for i = 0:(NUM_OF_HOURS-1)
+for i = 1:size(dates_unique, 1)
     
-    % get data from selected hour
-    hourly_temp = temp(time >= start+hours(i) & time < start+hours(i + 1), :);
-    times(i+1) = start+hours(i) + minutes(30);
-    
-    plot_internet_temp(i+1) = mean(internet_temp(internet_times >= start+hours(i) & internet_times < start+hours(i + 1)));
-    
-    % find Q1, Q2 Q3
+    selected_temperatures = temp(dates == dates_unique(i), :);
+
     for j = 1:NUM_OF_SENSORS
         
-       sensor = hourly_temp(~isnan(hourly_temp(:, j)), j);
+        sensor = selected_temperatures(~isnan(selected_temperatures(:, j)), j);
        
-       % get Q1, Q2, Q3
-       q1 = prctile(sensor, 25);
-       median_temp(i+1, j) = median(sensor);
-       q3 = prctile(sensor, 75);
+        % get Q1, Q2, Q3
+        q1 = prctile(sensor, 25);
+        median_temp(i, j) = median(sensor);
+        q3 = prctile(sensor, 75);
  
-       if ~isempty(sensor)
-           
-           % get IRQ
-           IRQ = abs(q3-q1);
+        if ~isempty(sensor)
 
-           pos_err(i+1, j) = abs(median_temp(i+1, j) - max(sensor(sensor <= q3+3/2*IRQ)));
-           neg_err(i+1, j) = abs(median_temp(i+1, j) - min(sensor(sensor >= q1-3/2*IRQ)));
-       end
-       
+            % get IRQ
+            IRQ = abs(q3-q1);
+            pos_err(i, j) = abs(median_temp(i, j) - max(sensor(sensor <= q3+3/2*IRQ)));
+            neg_err(i, j) = abs(median_temp(i, j) - min(sensor(sensor >= q1-3/2*IRQ)));
+        
+        end
     end
-    
 end
 
 % plot data
-bar(times, plot_internet_temp, 1, "FaceAlpha",0.5, 'linestyle', '--');
-% plot(times, plot_internet_temp , "-s", "color", "#4DBEEE", "linewidth", 20);  
+stairs(dates_int, internet_temp , "--", "color", "#000000", "linewidth", 2);  
 hold on
-% errorbar([times,times,times,times], median_temp,lower_outlier, upper_outlier, '-o', "linewidth", 1.5, "MarkerSize",8, "capsize", 10);
-errorbar(repmat(times, [1, NUM_OF_SENSORS]), median_temp,neg_err, pos_err, '-o', "linewidth", 1.5, "MarkerSize",8, "capsize", 10);
+errorbar(repmat(dates_unique, [1, NUM_OF_SENSORS]), median_temp, neg_err, pos_err, '-o', "linewidth", 1.5, "MarkerSize",8, "capsize", 10);
 hold off
 grid minor
 xlabel("date");
-xticks(times(1:3:NUM_OF_HOURS));
+xticks(dates_unique(1:3:NUM_OF_HOURS));
 ylabel("temperature [\circC]");
-legend(["Internet", "SHT31", "BMP280", "DS18B20", "DHT11", "DHT22"], "Location", "eastoutside")
+legend(["Internet", "SHT31", "BMP280", "DHT11", "DHT22", "DS18B20"], "Location", "eastoutside")
 title("Last 24 hour temperature");
